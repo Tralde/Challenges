5603cb2115f83b7bb0d4240801523503c18c7d6c
Tralde <118253841+Tralde@users.noreply.github.com>
2026-05-01 23:39:10 +0200

    Update PlayerChunkRandomEffectChallenge.java

--- plugin/src/main/java/net/codingarea/challenges/plugin/challenges/implementation/challenge/multiplayer/PlayerChunkRandomEffectChallenge.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/challenges/implementation/challenge/multiplayer/PlayerChunkRandomEffectChallenge.java
@@ -1,1 +1,35 @@
+public class PlayerChunkRandomEffectChallenge extends PlayerRandomizerSetting {
 
+    private Map<UUID, PotionEffect> currentPotionEffects = new HashMap<>();
+    private long worldSeed;
+
+    public PlayerChunkRandomEffectChallenge() {
+        super(MenuType.CHALLENGES);
+    }
+
+    @NotNull
+    @Override
+    public ItemBuilder createDisplayItem() {
+        return new ItemBuilder(
+                Material.POTION,
+                Message.forName("item-player-chunk-effect-challenge")
+        );
+    }
+
+    @Override
+    protected void onEnable() {
+        currentPotionEffects = new HashMap<>();
+        worldSeed = ChallengeAPI.getGameWorld(World.Environment.NORMAL).getSeed();
+
+        broadcastFiltered(player -> addEffect(player, player.getLocation()));
+    }
+
+    private PotionEffect getEffect(Player player, Chunk chunk) {
+        PotionEffectType[] types = PotionEffectType.values();
+
+        Random random = new Random(createPlayerChunkSeed(player, chunk, worldSeed));
+        PotionEffectType type = types[random.nextInt(types.length)];
+
+        return type.createEffect(Integer.MAX_VALUE, random.nextInt(type.isInstant() ? 1 : 4));
+    }
+}
